7c80cf0050a173655435d6753e1663242f24970f
Taylor <johnsoct@hey.com>
2026-02-18 07:38:26 -0500

    added newline

--- node/global-packages.sh
+++ node/global-packages.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+
 pnpm add -g @vue/language-server @vue/typescript-plugin corepack css-variables-language-server eslint eslint_d fixjson htmlhint jiti jsonlint markdownlint postcss-html sass stylelint stylelint-config-standard stylelint-config-standard-scss stylelint-lsp tldr typescript typescript-language-server vscode-css-languageservice vscode-html-languageservice vscode-json-languageservice vscode-langservers-extracted
