f0a02c9a4699f89d907f6e970da7192640d9add7
Taylor <johnsoct@hey.com>
2026-04-06 14:16:33 -0400

    updated treesitter to lazyvim'

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -5,66 +5,60 @@ return {
         -- ensures treesitter is rebuilt whenever we download new queries (parsers)
         build = ":TSUpdate",
         branch = "master",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-                auto_install = false,
-                ensure_installed = {
-                    "bash",
-                    "blade",
-                    "c",
-                    "css",
-                    "javascript",
-                    "go",
-                    "html",
-                    "jsdoc",
-                    "json",
-                    "lua",
-                    "query",
-                    "markdown",
-                    "markdown_inline",
-                    "php",
-                    "php_only",
-                    "scss",
-                    "sql",
-                    "toml",
-                    "typescript",
-                    "vim",
-                    "vimdoc",
-                    "vue",
-                    "yaml",
-                },
-                -- Automatically install missing parsers when entering buffer
-                highlight = {
-                    enable = true,
+        lazy = false,
+        opts = {
+            -- https://www.lazyvim.org/plugins/treesitter#nvim-treesitter
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = false,
+            folds = { enable = true },
+            ensure_installed = {
+                "bash",
+                "blade",
+                "c",
+                "css",
+                "javascript",
+                "go",
+                "html",
+                "jsdoc",
+                "json",
+                "lua",
+                "query",
+                "markdown",
+                "markdown_inline",
+                "php",
+                "php_only",
+                "scss",
+                "sql",
+                "toml",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "vue",
+                "yaml",
+            },
+            highlight = {
+                enable = true,
 
-                    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-                    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-                    -- the name of the parser)
-                    -- list of language that will be disabled
-                    -- disable = { "c", "rust" },
-                    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-                    disable = function(lang, buf)
-                        local max_filesize = 1000 * 1024 -- 1 MB
-                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                        if ok and stats and stats.size > max_filesize then
-                            return true
-                        end
-                    end,
+                -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+                -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+                -- the name of the parser) list of language that will be disabled
+                -- disable = { "c", "rust" },
+                -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+                disable = function(lang, buf)
+                    local max_filesize = 5000 * 1024 -- 5 MB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
 
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
-                    additional_vim_regex_highlighting = false,
-                },
-                indent = { enable = true },
-                sync_install = false,
-            })
-
-            vim.opt.foldmethod = "expr"
-            vim.opt.foldexpr = "nvim-treesitter#foldexpr()"
-        end,
+                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                -- Instead of true it can also be a list of languages
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = true },
+        },
     },
 }
