7a60247d35feaf4428397bd1ae023d6a6426e34d
Taylor <johnsoct@hey.com>
2026-04-23 10:45:20 -0400

    added hammerspoon init.lua 1-0

---
+++ hammerspoon/init.lua
@@ -0,0 +1,49 @@
+-- Brave Browser
+hs.hotkey.bind({ "cmd" }, "1", function()
+	hs.application.launchOrFocus("Brave Browser")
+end)
+
+-- Terminal
+hs.hotkey.bind({ "cmd" }, "2", function()
+	hs.application.launchOrFocus("Terminal")
+end)
+
+-- Claude
+hs.hotkey.bind({ "cmd" }, "3", function()
+	hs.application.launchOrFocus("Claude")
+end)
+
+-- Claude
+hs.hotkey.bind({ "cmd" }, "4", function()
+	hs.application.launchOrFocus("Postman")
+end)
+
+-- Claude
+hs.hotkey.bind({ "cmd" }, "5", function()
+	hs.application.launchOrFocus("Notion")
+end)
+
+-- Claude
+hs.hotkey.bind({ "cmd" }, "6", function()
+	hs.application.launchOrFocus("Google Chrome")
+end)
+
+-- Claude
+hs.hotkey.bind({ "cmd" }, "7", function()
+	hs.application.launchOrFocus("Microsoft Teams")
+end)
+
+-- Claude
+hs.hotkey.bind({ "cmd" }, "8", function()
+	hs.application.launchOrFocus("Microsoft Visio")
+end)
+
+-- Claude
+hs.hotkey.bind({ "cmd" }, "9", function()
+	hs.application.launchOrFocus("Microsoft Outlook")
+end)
+
+-- Claude
+hs.hotkey.bind({ "cmd" }, "0", function()
+	hs.application.launchOrFocus("Spotify")
+end)
