a2a3c5e7e7765abed92668b873bcde982ac45a3e
Taylor <johnsoct@hey.com>
2026-04-24 22:37:43 -0400

    swapped to tokyobones_light theme

--- nvim/lua/config/plugins/colorscheme.lua
+++ nvim/lua/config/plugins/colorscheme.lua
@@ -1,35 +1,8 @@
 return {
+    -- https://vimcolorschemes.com/i/trending
     {
-        -- https://github.com/ramojus/mellifluous.nvim
         {
-            "ramojus/mellifluous.nvim",
-            enabled = false,
-            config = function()
-                require("mellifluous").setup({
-                    mellifluous = {
-                        neutral = true,
-                    },
-                })
-                vim.cmd.colorscheme("mellifluous")
-            end,
-        },
-        -- https://github.com/davidosomething/vim-colors-meh?tab=readme-ov-file
-        {
-            "davidosomething/vim-colors-meh.nvim",
-            enabled = false,
-            config = function()
-                vim.cmd.colorscheme("meh")
-            end,
-        },
-        {
-            "mellow-theme/mellow.nvim",
-            enabled = false,
-            config = function()
-                -- Plug 'mellow-theme/mellow.nvim'
-                vim.cmd.colorscheme("mellow")
-            end,
-        },
-        {
+            -- https://vimcolorschemes.com/zenbones-theme/zenbones.nvim
             "zenbones-theme/zenbones.nvim",
             -- Optionally install Lush. Allows for more configuration or extending the colorscheme
             -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
@@ -40,7 +13,8 @@ return {
             -- https://github.com/zenbones-theme/zenbones.nvim
             config = function()
                 vim.g.zenbones_darken_comments = 45
-                vim.cmd.colorscheme("zenbones")
+                vim.o.background = "light"
+                vim.cmd.colorscheme("tokyobones")
             end,
         },
     },
